3c8127fa9f702c01af7b44d558dacda6d1089858
Muhim Muktadir Khan <Muhim Muktadir Khan>
2016-11-20 18:50:02 +0600

    initial code after making tree

---
+++ ForTestCode.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+
+#define MAX 1000
+
+int a,b,c;
+
+void hudai()
+{
+
+	for(int i=0; i<5; i++)
+	{
+		cout << "this is " << i << endl;
+	}
+
+	cout << endl;
+	cout << endl;
+}
+
+int main()
+{
+	printf("Hello World");
+
+	for(int i=0;i<10;i++){
+		cout<<"my name is zim";
+		if(MAX<i){
+			cout<<"impossible"<<endl;
+		}
+	}
+
+	return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,211 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+
+#define MAX 10000
+#define pb push_back
+
+struct node{
+	string type;
+	string code_segment;
+	int pos;
+	int parent;
+	vector<int>child;
+}root;
+
+
+
+vector<string> codes;
+fstream myFile;
+int node_idx;
+vector<node> noded_code;
+vector<int> tree[MAX];
+
+
+string supported_char ="();{}''\"+=-*&|#%^?/<>,";
+bool isChar(char x){
+	if((x>='A' && x<='Z') || (x>='a' && x<='z')) return true;
+	for(int i=0;i<supported_char.size();i++){
+		if(x == supported_char[i]) return true;
+	}
+	return false;
+}
+
+bool starts_with(string x,string with){
+	if(x.size()>with.size()) return false;
+	
+	for(int i=0;i<x.size();i++){
+		if(x[i]!=with[i]) return false;
+	}
+	return true;
+	
+}
+
+
+
+void store()
+{
+	string line;
+
+    if (myFile.is_open())
+    {
+   	    while ( getline (myFile,line) )
+    	{
+      		// cout << line << '\n';
+    		if(line.size()<1) continue;
+      		codes.push_back(line + "\n");
+    	}
+    
+    }
+
+}
+
+void printVector()
+{
+	for(int i=0; i<codes.size(); i++)
+    {
+
+    	cout << codes.at(i)<<endl;;
+    }
+}
+
+
+
+
+node create_node(string line){
+	
+	node temp;
+	temp.pos = node_idx++;
+
+	if(starts_with("#include",line)==true){
+		temp.type="header";
+	}
+	else if(starts_with("#define",line)==true){
+		temp.type="macro";
+	}
+	else if(line[line.length()-1]==';'){
+		temp.type = "statement";
+	}
+	else if(line.length()==1){
+		if(line[0]=='{'){
+			temp.type = "start_brace";
+		}
+		else temp.type="close_brace";
+	}
+	else if(line[line.length()-1]==')' || line[line.length()-1]=='{' || line[line.length()-1]=='e'){
+		if(starts_with("for",line)==true || starts_with("while",line)==true || starts_with("do",line)==true){
+			temp.type = "loop";
+		}
+		else if(starts_with("if",line)==true || starts_with("else",line)==true){
+			temp.type= "condition";
+		}
+		else{
+			temp.type = "function";
+		}
+	}
+	return temp;
+
+}
+
+
+void dfs(int cur){
+	if(tree[cur].size()>0) 
+		cout<<cur<<" -> ";
+	for(int i=0;i<tree[cur].size();i++){
+		cout<<tree[cur][i]<<' ';
+	}
+	if(tree[cur].size()>0)
+		cout<<endl;
+	for(int i=0;i<tree[cur].size();i++){
+		if(tree[cur][i]!=cur) dfs(tree[cur][i]);
+	}
+
+}
+
+
+
+void generate_tree(){
+	
+
+	int cur = 0;
+
+	for(int i=0;i<codes.size();i++){
+		string line = codes[i];
+		node temp = create_node(line);
+		noded_code.pb(temp);
+		cout<<line<<" -> "<<noded_code[i].pos<<endl;
+
+		if(temp.type == "loop" || temp.type == "condition" || temp.type =="function"){
+			noded_code[i].parent = cur;
+			tree[cur].pb(temp.pos);
+			cur = temp.pos;
+		}
+		else if(temp.type == "close_brace"){
+			cur = noded_code[cur].parent;
+		}
+		else{
+			tree[cur].pb(temp.pos);
+			noded_code[i].parent=cur;
+		}
+
+
+
+	}
+	cout<<endl;
+	cout<<"Generated tree: "<<endl;
+	dfs(0);
+
+
+
+}
+
+
+
+void trim()
+{	
+	for(int j=0; j<codes.size(); j++)
+    {
+
+    	string line;
+    	line = codes[j];
+
+    	for(int i=0; i<line.size(); i++)
+    	{
+    		if(isChar(line[i])==true && i!=0){
+    			line.erase(0,i);
+    			break;
+    		}
+    		else if(isChar(line[i])==true) {
+				break;
+			}
+    	}
+    	for(int i=line.length()-1;i>=0;i--){
+    		if(isChar(line[i])==false){
+    			line.erase(i,1);
+    		}
+    		if(isChar(line[i])==true) break;
+    	}
+    	codes[j]=line;
+    }
+    // printVector();
+}
+
+
+int main()
+{
+	
+	myFile.open("ForTestCode.c");
+
+	store();
+    // printVector();
+    trim();
+    generate_tree();
+
+    myFile.close();
+
+    return 0;
+
+}
